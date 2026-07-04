24933466c9dc284c3391b4a5554c175af6404bd7
Haru398 <Haru398@users.noreply.github.com>
2026-07-05 07:06:11 +0900

    fix(seo): explicitly allow Yeti and Googlebot in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,10 +4,20 @@ export const dynamic = 'force-static';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'Yeti',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+      }
+    ],
     sitemap: 'https://item.monster/sitemap.xml',
   };
 }
